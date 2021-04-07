3497d6a85644d8ff7f9c033221c9ca089d8324cb
Anirudhx234 <65822278+Anirudhx234@users.noreply.github.com>
2021-04-06 20:03:58 -0500

    Added Doors(Doesn't properly work)
    title

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Sponsors from './components/sponsors';
 import './app.css';
 
 import { ReactComponent as Duck } from './images/logo-2021.svg';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.sponsors = React.createRef();
+    }
     render() {
         return (
             <div className="app">
                 <Duck className="logo"></Duck>
+                <div className="more"></div>
+                <Sponsors reference={this.sponsors} />
+                <div className="more"></div>
+                <div className="more"></div>
             </div>
         );
     }
