86c0c7f2b6e3ce9e6024af6b77071c0995a9570b
Adheesh Kadiresan <64273007+exploringsci@users.noreply.github.com>
2021-05-12 21:33:22 -0500

    Used faq-box class for each faq and placed it in the proper file layout/format

--- src/components/faq.js
+++ src/components/faq.js
@@ -3,19 +3,46 @@ import faqs from './faq.json';
 import FAQBox from './faq-box';
 import './faq.css';
 
-const FAQ = (props) => (
-    <section>
-        <div className="wrap">
-            <h2>
-                FAQs
-            </h2>
-            {
-                faqs.map(v => (
-                    <FAQBox question={v.question} answer={v.answer} />
-                ))
-            }
-        </div>
-    </section>
-)
+class FAQ extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { dropdown: false, toggle: 'left' };
+    }
+
+    toggleDropdown = () => {
+        this.setState({ dropdown: !this.state.dropdown });
+    };
+
+    changeSwitch = (dir) => {
+        this.setState({ toggle: dir });
+    };
+
+    render() {
+        return (
+            <div className="event">
+                <img
+                    className="event-header"
+                    alt={`${this.props.year} header`}
+                    src={`${imageDb}/${this.props.year}.png`}
+                    onClick={this.toggleDropdown}
+                ></img>
+                <div className={`event-dropdown ${this.state.dropdown ? 'active' : ''}`}>
+                    <p className="event-dropdown-info">{info}</p>
+                    <div className="event-dropdown-button-container">
+                        <div className="event-dropdown-switch-container">
+                            <div className={`event-dropdown-switch-back ${this.state.toggle}`}></div>
+                        </div>
+                    </div>
+                    <div className={`event-dropdown-section organizers ${this.state.toggle}`}>
+                        <div className="event-organizers-list">{organizersList}</div>
+                    </div>
+                    <div className={`event-dropdown-section sponsors ${this.state.toggle}`}>
+                        <div className="event-sponsors">Sponsors WIP</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default FAQ;
