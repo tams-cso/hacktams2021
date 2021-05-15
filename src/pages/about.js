a76f92d179976ec9b3f779fc3a00674c7be721ba
DylanLi272 <56173725+DylanLi272@users.noreply.github.com>
2021-05-15 00:39:47 -0500

    Clean up website + font & color fixes (#14)
    * Create basic components
    
    * Rename App.js to app.js
    
    * Rename App.css to app.css
    
    * Add basic speakers section framework
    
    * Create data folder
    
    * Add more in speakers section + minor page organization
    
    * Small changes to css and code format
    
    * Reorganize files + fix-ish background color
    
    * Home section background color change
    
    * Reformat all files
    
    * Clean up website + font & color fixes
    
    Co-authored-by: The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,7 +18,7 @@ class About extends React.Component {
     render() {
         return (
             <div className="about">
-                <h2>About</h2>
+                <h1>About</h1>
                 <p>
                     hackTAMS is an annual hackathon organized by students at the Texas Academy of Mathematics and
                     Science. Our event brings high school students from across the DFW area to showcase their talent and
