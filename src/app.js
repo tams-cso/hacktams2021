d1f3e08275b9335433bb1555c9e6127d1d75a195
DylanLi272 <56173725+DylanLi272@users.noreply.github.com>
2021-05-28 16:00:20 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Home from './pages/home';
 import About from './pages/about';
+import Speakers from './pages/speakers';
 import FAQ from './pages/faq';
 import Sponsors from './pages/sponsors';
 import Footer from './pages/footer';
 import './app.css';
-
-import { ReactComponent as Duck } from './images/logo-2021.svg';
-import Speakers from './pages/speakers';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.home = React.createRef();
         this.about = React.createRef();
+        this.speakers = React.createRef();
         this.faq = React.createRef();
         this.sponsors = React.createRef();
         this.footer = React.createRef();
