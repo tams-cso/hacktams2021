e81ac70508b04233d76c3a150c5f7ae1161ef510
Anirudhx234 <65822278+Anirudhx234@users.noreply.github.com>
2021-10-10 00:49:38 -0500

    made about section changes and reworked faq. fix spacing for faq question one and find way to keep box shadow on trackk switch

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import data from '../data/faqData.json';
-import FAQBox from '../components/faq-box';
+import { FAQBox } from '../components/faq-box';
 import './faq.css';
 
 class FAQ extends React.Component {
@@ -8,18 +8,18 @@ class FAQ extends React.Component {
         return (
             <div className="faq" id="faq">
                 <h1>FAQ</h1>
-                <div className="q_and_a">
-                    <div className="col-left">
-                        <FAQBox key={data[0].question} question={data[0].question} answer={data[0].answer} />
-                        <FAQBox key={data[1].question} question={data[1].question} answer={data[1].answer} />
-                        <FAQBox key={data[2].question} question={data[2].question} answer={data[2].answer} />
-                        <FAQBox key={data[3].question} question={data[3].question} answer={data[3].answer} />
+                <div className="faq-container">
+                    <div className="faq-side left">
+                        <FAQBox color="#3ABFEF" question={data[0].question} answer={data[0].answer} />
+                        <FAQBox color="#3ABFEF" question={data[1].question} answer={data[1].answer} />
+                        <FAQBox color="#3ABFEF" question={data[2].question} answer={data[2].answer} />
+                        <FAQBox color="#3ABFEF" question={data[3].question} answer={data[3].answer} />
                     </div>
-                    <div className="col-right">
-                        <FAQBox key={data[4].question} question={data[4].question} answer={data[4].answer} />
-                        <FAQBox key={data[5].question} question={data[5].question} answer={data[5].answer} />
-                        <FAQBox key={data[6].question} question={data[6].question} answer={data[6].answer} />
-                        <FAQBox key={data[7].question} question={data[7].question} answer={data[7].answer} />
+                    <div className="faq-side right">
+                        <FAQBox color="#6270F0" question={data[4].question} answer={data[4].answer} />
+                        <FAQBox color="#6270F0" question={data[5].question} answer={data[5].answer} />
+                        <FAQBox color="#6270F0" question={data[6].question} answer={data[6].answer} />
+                        <FAQBox color="#6270F0" question={data[7].question} answer={data[7].answer} />
                     </div>
                 </div>
             </div>
