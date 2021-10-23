5367c18a8c4b18aab5ec105e8aa87124b72fc97f
DylanLi272 <56173725+DylanLi272@users.noreply.github.com>
2021-10-23 04:51:19 -0500

    Add sponsors (#37)

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import Frame from '../images/sponsor_frame.svg';
 /*import Default from '../images/raw.png';*/
+import data from '../data/sponsorData.json';
 import './sponsors.css';
 
+const imageDB = 'https://hacktams-backend.s3.us-east-2.amazonaws.com';
+
 class Sponsors extends React.Component {
+    createSponsors = () => {
+        const sponsorList = [];
+        data.forEach((sponsor) => {
+            sponsorList.push(
+                <img
+                    src={`${imageDB}/sponsors/fall2021/${sponsor.img}`}
+                    className={`sponsor${sponsor.tier == 1 ? ' tier1' : ''}`}
+                    alt={sponsor.name}
+                    onClick={() => {
+                        window.open(sponsor.link, '_blank');
+                    }}
+                />
+            );
+            console.log(`${imageDB}/sponsors/fall2021/${sponsor.img}`);
+        });
+        return sponsorList;
+    };
+
     render() {
+        const sponsors = this.createSponsors();
         return (
             <div className="sponsors" id="sponsors">
                 <h1>Our Sponsors</h1>
-                <div className="break"></div>
-                <img src={Frame} alt="Frame" className="sponsor large" />
-                <div className="break"></div>
-                <img src={Frame} alt="Frame" className="sponsor medium" />
-                <img src={Frame} alt="Frame" className="sponsor medium" />
-                <img src={Frame} alt="Frame" className="sponsor small" />
-                <img src={Frame} alt="Frame" className="sponsor small" />
-                <img src={Frame} alt="Frame" className="sponsor small" />
+                <div className="sponsors-list">{sponsors}</div>
             </div>
         );
     }
