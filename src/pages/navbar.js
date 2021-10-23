a4cd72e32958f02e12fb618e2b61c0154ca6f272
DylanLi272 <56173725+DylanLi272@users.noreply.github.com>
2021-10-23 05:44:45 -0500

    Redo Navbar (#41)

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -4,46 +4,32 @@ import './navbar.css';
 import Logo from '../images/logo-2021.svg';
 
 class Navbar extends React.Component {
-    openSite = (site) => {
-        if (site !== '') window.open(site);
-    };
-
     render() {
         return (
             <div className="navbar" id="navbar">
-                <header>
-                    <a className="logo" href="/#navbar">
-                        <img src={Logo} alt="logo" className="logoimg" />
+                <a className="nav-link logo" href="/#navbar">
+                    <img src={Logo} alt="logo" className="logoimg" />
+                </a>
+                <div className="navbar-container">
+                    <a className="nav-link" href="/#about">
+                        About
                     </a>
-                    <nav>
-                        <ul className="nav__links">
-                            <li>
-                                <a className="nav-link" href="/#about">
-                                    About
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav-link" href="/#speakers">
-                                    Speakers
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav-link" href="/#faq">
-                                    FAQ
-                                </a>
-                            </li>
-                            <li>
-                                <a className="nav-link" href="/#footer">
-                                    Contact Us
-                                </a>
-                            </li>
-                            {/* <li><a href="/#sponsors">Sponsors</a></li> */}
-                        </ul>
-                    </nav>
-                    <a className="nav-hide" href="/#navbar">
-                        nothing
+                    <a className="nav-link" href="/#speakers">
+                        Speakers
                     </a>
-                </header>
+                    <a className="nav-link" href="/#faq">
+                        FAQ
+                    </a>
+                    <a className="nav-link" href="/#footer">
+                        Contact Us
+                    </a>
+                    <a className="nav-link" href="/#sponsors">
+                        Sponsors
+                    </a>
+                </div>
+                <a className="logo filler" href="/#navbar">
+                    <img src={Logo} alt="logo" className="logoimg" />
+                </a>
             </div>
         );
     }
